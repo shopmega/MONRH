7716c8e54e6cf518968d41ba8dc7a24605c8c761
Zouhair <zouhair@users.noreply.github.com>
2026-02-15 18:53:03 +0000

    Fix ads.txt delivery for Vercel and bypass proxy matcher

--- src/proxy.ts
+++ src/proxy.ts
@@ -58,6 +58,6 @@ export async function proxy(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|opengraph-image|twitter-image).*)",
+    "/((?!_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|opengraph-image|twitter-image|ads.txt).*)",
   ],
 };
